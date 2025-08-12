5cb3540bdef9a56bdeac2d5273b3125f115ca6d9
hughbuntine <hughbuntine@gmail.com>
2025-08-12 15:54:22 +1000

    updated dumumub-0000004 bio

--- frontend/src/COMPONENTS/Banner/Banner.jsx
+++ frontend/src/COMPONENTS/Banner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
     const button1info = "dumumub is an audio plug-in creation project run by a human person\nthe project intends to provide experimental audio plug-ins for musicians to utilize to produce and/or perform interesting sounds and/or music\nthe plug-ins are free\nenjoy";
 
     const button2Text = "plugins";
-    const button2info = "dumumub-0000003:\n\na wavetable synthesizer that allows the user to create and manipulate wavetables by dragging and dropping audio files and images or drawing in their wave directly onto the plugin";
+    const button2info = "dumumub-0000003:\n\na wavetable synthesizer that allows the user to create and manipulate wavetables by dragging and dropping audio files and images or drawing in their wave directly onto the plug-in\n\n\ndumumub-0000004:\n\na frequency manipulation tool that allows the user to drag and drop frequency information of an incoming signal in real time\n\nmore plugins coming soon";
 
     const button3Text = "contact";
     const button3info = "email:\nhi@dumumub.com\n\ninstagram:\n@dumumub";
